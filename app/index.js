72244b0fbcf3892cbfcb0f6ba7a646a407942527
Vandal-William <vandal.william@hotmail.com>
2023-02-24 23:17:56 +0100

    fixe bug content type, change port to 80

--- app/index.js
+++ app/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 const app = express();
 
+// due a un probleme de mineType qui bloquais l'accés au css et au js, j'ai du aller
+// dans le html et ajouter dans la balise link {type="text/css"}  et dans la balise scripte  {type="text/javascript"}
+// puis dans le app.use j'ai ajouter un objet:
 app.use(express.static("build", {
-    index: false, 
-    immutable: true, 
-    cacheControl: true,
-    maxAge: "30d"
+    index: false, // désactive la réponse automatique avec le fichier index.html si aucun nom de fichier n'est spécifié.
+    immutable: true, // ajoute l'en-tête immutable pour les fichiers statiques servis. Cela indique aux navigateurs que le contenu ne changera jamais, ce qui leur permet de mettre en cache le contenu plus longtemps.
+    cacheControl: true, // ajoute l'en-tête Cache-Control pour les fichiers statiques servis. Cela spécifie comment les navigateurs doivent mettre en cache le contenu.
+    maxAge: "30d" // indique à Express de mettre à jour automatiquement les fichiers statiques mis en cache après 30 jours. Cela permet de s'assurer que les navigateurs reçoivent toujours du contenu à jour.
+    // En résumé, ce code permet de servir des fichiers statiques à partir d'un répertoire spécifié avec une mise en cache optimisée pour les performances et une durée de cache maximale de 30 jours.
 }));
 
 app.get('/', (req, res) => {
