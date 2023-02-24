d2e4247b61c7678d76bc4b5177d9701ccb0a69a2
Vandal-William <vandal.william@hotmail.com>
2023-02-24 21:48:00 +0100

    error path

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static("../build"));
 
 app.get('/', (req, res) => {
-    res.sendFile("../build/index.html");
+    res.sendFile(__dirname + "../build/index.html");
   });
 
    
